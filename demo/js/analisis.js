96f54d1e5e1c5e137caad577abd52c118a436325
lsantiago <lsquinones@gmail.com>
2018-01-26 14:25:08 -0500

    Update Demo

--- demo/js/analisis.js
+++ demo/js/analisis.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	cargarJSON();
+	
 
 	$('#btnSumar').click(function(){
 		var resultado = parseFloat($('#txtPrimerNum').val()) + parseFloat($('#txtSegundoNum').val()); 
@@ -9,8 +9,15 @@ $(document).ready(function(){
 		var Gs = [23.4, 34.5, 12];
 		var U = [23.4, 34.5, 12];
 		console.log(Gs[1] + U[2]);
-		$('#txtResultado').val(Gs[0]);
 
+
+		$('#txtResultado').val(resultado);
+
+	});
+
+
+	$('#get-data').click(function () {
+		cargarJSON();
 	});
 
 
@@ -20,7 +27,21 @@ $(document).ready(function(){
 function cargarJSON(){
 	console.log('Cargando JSON');
 
+	var showData = $('#show-data');
+
 	$.getJSON('./data/db.json', function (data) {
 		console.log(data);
+
+		var items = data.items.map(function (item) {
+			return item.key + ': ' + item.value;
+		});
+
+		showData.empty();
+
+		if (items.length) {
+			var content = '<li>' + items.join('</li><li>') + '</li>';
+			var list = $('<ul />').html(content);
+			showData.append(list);
+		}
 	});
 }
