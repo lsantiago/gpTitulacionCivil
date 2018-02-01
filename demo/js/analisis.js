1098a592359c402b8996fef402ce6da6b54a1f04
lsantiago <lsquinones@gmail.com>
2018-02-01 15:46:44 -0500

    Add BD

--- demo/js/analisis.js
+++ demo/js/analisis.js
@@ -17,7 +17,19 @@ $(document).ready(function(){
 
 
 	$('#get-data').click(function () {
-		cargarJSON();
+		//cargarJSON();
+		loadBinaryFile('data/vlee.sqlite', function(data){
+			console.log("Loading database...");
+			var db = new SQL.Database(data);
+
+			var res = db.exec("SELECT id FROM Factor WHERE id=1");
+			document.getElementById("res").textContent = JSON.stringify(res);
+
+			valor = JSON.parse(JSON.stringify(res));
+			console.log(valor);
+			
+			//console.log(parseFloat(valor[0].values[0]) +2);
+		});
 	});
 
 
@@ -45,3 +57,20 @@ function cargarJSON(){
 		}
 	});
 }
+
+
+// extrae información del archivo sqlite
+function loadBinaryFile(path,success) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", path, true); 
+	xhr.responseType = "arraybuffer";
+	xhr.onload = function() {
+		var data = new Uint8Array(xhr.response);
+		var arr = new Array();
+		for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+			success(arr.join(""));
+	};
+	xhr.send();
+};
+
+
